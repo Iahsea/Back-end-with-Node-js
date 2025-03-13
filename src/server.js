c105f38f861ed11c5b6e977f8f5eb02f0476ee6b
Iahsea <ngyenchinhhai@gmail.com>
2025-03-14 01:48:57 +0700

    video 18

--- src/server.js
+++ src/server.js
@@ -1,6 +1,11 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const path = require("path");
+const app = express();
 const port = 8080
+
+//config template engine
+app.set("views", path.join(__dirname, 'views'));
+app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
@@ -11,7 +16,8 @@ app.get('/abc', (req, res) => {
 })
 
 app.get('/iahsea', (req, res) => {
-    res.send('<h1>Iahsea</h1>')
+    // res.send('<h1>Iahsea</h1>')
+    res.render('sample.ejs')
 })
 
 app.listen(port, () => {
