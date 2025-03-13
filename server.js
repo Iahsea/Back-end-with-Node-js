0d778924709bc85b982526845e9cac643461139b
Iahsea <ngyenchinhhai@gmail.com>
2025-03-14 00:16:14 +0700

    video 14 express

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-const { createServer } = require('node:http');
-const hostname = 'localhost';
-const port = 3000;
-const server = createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World with Iahsea');
-});
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
-});
+const express = require('express')
+const app = express()
+const port = 8080
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
