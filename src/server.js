90509bcbc3f46a80de8a72a1715634593722f2fe
Iahsea <ngyenchinhhai@gmail.com>
2025-03-16 10:47:00 +0700

    video 30 mysql with node js

--- src/server.js
+++ src/server.js
@@ -1,8 +1,8 @@
 require('dotenv').config();
 const express = require('express');
-const path = require("path");
 const configViewEngine = require('./config/viewEngine');
 const webRoutes = require('./routes/web');
+const mysql = require('mysql2');
 
 console.log(">>> check env: ", process.env);
 
@@ -16,7 +16,28 @@ const hostname = process.env.HOST_NAME;
 
 configViewEngine(app);
 
+//khai báo route
+
 app.use('/', webRoutes);
+
+//test connection
+//create the connection to database
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3308, //default: 3306
+    user: 'root',
+    password: '123456',
+    database: 'iahsea'
+});
+
+// A simple SELECT query
+connection.query(
+    'select * from Users u',
+    function (err, results, fields) {
+        console.log(">>>results= ", results); // results contains rows returned by server
+        console.log(">>> fields= ", fields); // fields contains extra meta data about results, if available
+    }
+);
 
 
 app.listen(port, hostname, () => {
