a2ec488c9565095ca78cee7c5208a8abe2354e33
Iahsea <ngyenchinhhai@gmail.com>
2025-03-16 15:55:38 +0700

    video 38 Create a user with dynamic question mark parameters in mysql2

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -14,9 +14,30 @@ const getIahsea = (req, res) => {
 }
 
 const postCreateUser = (req, res) => {
-    console.log(">>> req.body", req.body);
 
-    res.send('create a new user')
+    let email = req.body.email;
+    let name = req.body.myname;
+    let city = req.body.city;
+
+    console.log(">>> check", email, name, city);
+
+    // let {email, name, city} = req.body;
+
+
+    // INSERT INTO Users(email, name, city)
+    // VALUES('iahsea@gmail.com', 'Nguyen Hai', 'ha tinh');
+
+    connection.query(
+        `INSERT INTO 
+        Users(email, name, city) 
+        VALUES (?, ?, ?);`,
+        [email, name, city],
+        function (err, results) {
+            res.send(' Created user succeed !')
+        }
+    );
+
+
 }
 
 module.exports = {
