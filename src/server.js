f4b5be3a07c6c937ee9944bfc3f01b44969ad920
Iahsea <ngyenchinhhai@gmail.com>
2025-03-16 11:09:55 +0700

    video 31 reuse connection

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const configViewEngine = require('./config/viewEngine');
 const webRoutes = require('./routes/web');
-const mysql = require('mysql2');
+const connection = require('./config/database');
 
 console.log(">>> check env: ", process.env);
 
@@ -20,22 +20,13 @@ configViewEngine(app);
 
 app.use('/', webRoutes);
 
-//test connection
-//create the connection to database
-const connection = mysql.createConnection({
-    host: 'localhost',
-    port: 3308, //default: 3306
-    user: 'root',
-    password: '123456',
-    database: 'iahsea'
-});
+
 
 // A simple SELECT query
 connection.query(
     'select * from Users u',
     function (err, results, fields) {
         console.log(">>>results= ", results); // results contains rows returned by server
-        console.log(">>> fields= ", fields); // fields contains extra meta data about results, if available
     }
 );
 
