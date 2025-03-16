d4f20af7449365aa2e9a78d10f50aeef5983a159
Iahsea <ngyenchinhhai@gmail.com>
2025-03-16 11:27:26 +0700

    video 32 some issues with connection node js

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,6 +1,23 @@
+const connection = require('../config/database');
+
 
 const getHomepage = (req, res) => {
-    res.send('Hello World vs Iahsea')
+    //process data
+    // call model 
+
+    let users = [];
+
+    connection.query(
+        'select * from Users u',
+        function (err, results, fields) {
+            users = results;
+            console.log(">>>results home page= ", results); // results contains rows returned by server
+
+            console.log(">> check users", users);
+            res.send(JSON.stringify(users))
+        }
+    );
+
 }
 
 const getABC = (req, res) => {
