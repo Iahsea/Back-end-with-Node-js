b003359935a4769d730e3c636908055af3d2fe84
Iahsea <ngyenchinhhai@gmail.com>
2025-03-21 22:25:02 +0700

    100 filter basic

--- src/services/customerService.js
+++ src/services/customerService.js
@@ -30,14 +30,23 @@ const createArrayCustomerService = async (arr) => {
     }
 }
 
-const getAllCustomerService = async (limit, page) => {
+const getAllCustomerService = async (limit, page, name) => {
     try {
         let result = null;
         if (limit && page) {
             let offset = (page - 1) * limit;
-            result = await Customer.find({ deleted: false }).skip(offset).limit(limit).exec();
+            if (name) {
+                result = await Customer.find(
+                    {
+                        "name": { $regex: '.*' + name + '.*' },
+                    }
+                ).skip(offset).limit(limit).exec();
+            } else {
+                result = await Customer.find({}).skip(offset).limit(limit).exec();
+            }
+
         } else {
-            result = await Customer.find({ deleted: false });
+            result = await Customer.find({});
         }
         return result;
     } catch (error) {
